58a3addfe727259aaec48b2b1e10cbeffb54cd04
billey_b <bastien.billey@gmail.com>
2018-05-24 00:15:27 +0200

    style style style

--- app/src/main/java/com/onionsquare/psyaround/feature/country/CountriesActivity.kt
+++ app/src/main/java/com/onionsquare/psyaround/feature/country/CountriesActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
+import android.view.View
 import com.onionsquare.psyaround.PsyApp
 import com.onionsquare.psyaround.R
 import com.onionsquare.psyaround.feature.BaseActivity
+import com.onionsquare.psyaround.feature.HeaderViewDecoration
 import com.onionsquare.psyaround.feature.parties.PartiesActivity
 import com.onionsquare.psyaround.model.Country
 import kotlinx.android.synthetic.main.countries.*
+import kotlinx.android.synthetic.main.header.view.*
 
 class CountriesActivity : BaseActivity(), CountriesView {
 
@@ -24,6 +27,11 @@ class CountriesActivity : BaseActivity(), CountriesView {
     }
 
     override fun showCountries(countries: List<Country>) {
+        val view = layoutInflater.inflate(R.layout.header, null)
+        view.name_header.text = getString(R.string.countries_title)
+        view.flag_header.visibility = View.GONE
+        countries_recycler.addItemDecoration(HeaderViewDecoration(view))
+
         val adapter = CountryAdapter(countries, object : CountryAdapter.CountryClickListener {
             override fun onClick(country: Country) {
                 val intent = Intent(this@CountriesActivity, PartiesActivity::class.java)
@@ -35,7 +43,7 @@ class CountriesActivity : BaseActivity(), CountriesView {
         countries_recycler.adapter = adapter
     }
 
-    override fun provideToolbarTitle(): String = getString(R.string.countries_title)
+    override fun provideToolbarTitle(): String = ""
 
     override fun provideLayout(): Int = R.layout.countries
 
